9c355fda4e7da8d4756decee8db2b75573ba441d
Cooper Brislain <cbrislain@gmail.com>
2019-12-02 13:25:01 -0800

    nearby search

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -4,11 +4,19 @@ const axios = require('axios');
 const config = require('./../config/keys');
 
 const miles_to_meters = x => x*1609.344;
+const meters_to_miles = x => x/1609.344;
 
 const convertZipToGeoCode = async (zipCode) => {
-    const { results } = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=${config.google_api_key}`);
-    const geoCode = results[0].geometry.location;
-    return geoCode;
+    const req_url = `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=${config.google_api_key}`;
+    console.log(req_url);
+    try {
+        const response = await axios.get(req_url);
+        const { results } = response.data;
+        const geoCode = results[0].geometry.location;
+        return geoCode;
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 module.exports = {
@@ -38,23 +46,24 @@ module.exports = {
         console.log(`SEARCHING FOR PART NAMED ${partName} WITHIN ${searchDistance} OF ${searchZip}`);
         const location = await convertZipToGeoCode(searchZip);
         const radius = miles_to_meters(searchDistance);
-        console.log(`IN OTHER WORDS: ${partId} WITHIN ${radius} OF ${location}`);
-        if (name) {
+        if (partName) {
             try {
-                const part = db.Part.find({name});
+                const part = await db.Part.findOne({ name: partName });
                 partId = part._id;
             } catch (e) {
                 res.json(e);
             }
         }
+        console.log(`IN OTHER WORDS: ${partId} WITHIN ${radius} OF ${location}`);
+        console.log(location);
         try {
-            const invItems = await db.Inventory.find({
-                'item._id': partId,
-                qty: { $gt: 0 }
-            });
+            const invItems = await db.Inventory.find().populate('item');
+            console.log(invItems);
             const nearItems = invItems.filter(invItem => {
                 const point = invItem.location;
-                console.log(geolib.getDistance(location, point));
+                console.log(point);
+                console.log(location);
+                console.log(meters_to_miles(geolib.getDistance(location, point)));
                 return geolib.getDistance(location, point) < radius;
             });
             res.json(nearItems);
